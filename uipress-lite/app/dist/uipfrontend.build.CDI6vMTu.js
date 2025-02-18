e679c3281f2672280fcf748d7257c9f75502eee1
Sean Murphy <sean@iamseanmurphy.com>
2025-02-18 16:19:40 +0000

    3.5.02

---
+++ uipress-lite/app/dist/uipfrontend.build.CDI6vMTu.js
@@ -0,0 +1,2 @@
+import{s as e}from"./assets/setupApp-CpS50Wbg.js";import"./assets/index-JRo3zg6O.js";const o=document.querySelector("#wpadminbar");if(o){const t=document.createElement("div");t.id="uip-frontend-toolbar",o.appendChild(t),e(t)}
+//# sourceMappingURL=uipfrontend.build.CDI6vMTu.js.map
