6c481fcd2654f4b126f3245fbaff7fa2e9862996
Sean Murphy <sean@iamseanmurphy.com>
2025-03-18 16:19:28 +0000

    3.5.06

---
+++ uipress-lite/app/dist/uipadminpage.build.BQr8Ivi-.js
@@ -0,0 +1,2 @@
+import{s as t}from"./assets/setupApp-CnNHhSMY.js";import{X as e}from"./assets/index-Dulbr5bF.js";let a=new URL(import.meta.url).searchParams.get("template-id");e.value.templateId=a;e.value.templateType="ui-admin-page";const m=document.querySelector("#uip-admin-page");t(m);
+//# sourceMappingURL=uipadminpage.build.BQr8Ivi-.js.map
---
+++ uipress-lite/app/dist/uipfrontend.build.DVjULPxa.js
@@ -0,0 +1,2 @@
+import{s as e}from"./assets/setupApp-CnNHhSMY.js";import"./assets/index-Dulbr5bF.js";const o=document.querySelector("#wpadminbar");if(o){const t=document.createElement("div");t.id="uip-frontend-toolbar",o.appendChild(t),e(t)}
+//# sourceMappingURL=uipfrontend.build.DVjULPxa.js.map
